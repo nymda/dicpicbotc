ffea35a4a7fddaf9f0778be4a736e25eceee68b0
jake hartley <puffleboy101@hotmail.co.uk>
2019-01-29 21:53:01 +0000

    --first commit

---
+++ dicpicbotc/Modules/Ping.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace dicpicbotc.Modules
+{
+    public class Ping : ModuleBase<SocketCommandContext>
+    {
+        [Command("ping")]
+        public async Task PingAsync()
+        {
+            await ReplyAsync("hello world!");
+        }
+    }
+}
--- dicpicbotc/Program.cs
+++ dicpicbotc/Program.cs
@@ -1,12 +1,81 @@
 ﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+
+//base by youtube codeforge
 
 namespace dicpicbotc
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) => new Program().RunBotAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient dSocket;
+        private CommandService dComm;
+        private IServiceProvider services;
+
+        public async Task RunBotAsync()
         {
-            Console.WriteLine("Hello World!");
+            dSocket = new DiscordSocketClient();
+            dComm = new CommandService();
+            services = new ServiceCollection()
+                .AddSingleton(dSocket)
+                .AddSingleton(dComm)
+                .BuildServiceProvider();
+
+            string bottoken = "NTM5OTE2NzY3MjI5NzcxODE0.DzJUlg.kknjp5J9kIpL3bmUEJo_XJXbyKs";
+
+            dSocket.Log += Log;
+
+            await RegisterCommandAsync();
+
+            await dSocket.LoginAsync(Discord.TokenType.Bot, bottoken);
+
+            await dSocket.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage arg)
+        {
+            Console.WriteLine(arg);
+            return Task.CompletedTask;
+        }
+
+        public async Task RegisterCommandAsync()
+        {
+            dSocket.MessageReceived += handleCommandAsync;
+
+            await dComm.AddModulesAsync(Assembly.GetEntryAssembly(), services);
+        }
+
+        private async Task handleCommandAsync(SocketMessage arg)
+        {
+            var message = arg as SocketUserMessage;
+
+            if (message is null || message.Author.IsBot) return;
+
+            int arguentPosition = 0;
+
+            if (message.HasStringPrefix("dp.", ref arguentPosition) || message.HasMentionPrefix(dSocket.CurrentUser, ref arguentPosition))
+            {
+                var context = new SocketCommandContext(dSocket, message);
+
+                var result = await dComm.ExecuteAsync(context, arguentPosition, services);
+
+                if (!result.IsSuccess)
+                {
+                    Console.WriteLine(result.ErrorReason);
+                }
+                if (result.IsSuccess)
+                {
+                    Console.WriteLine("responded to {0}", message.Author);
+                }
+            }
         }
     }
 }
